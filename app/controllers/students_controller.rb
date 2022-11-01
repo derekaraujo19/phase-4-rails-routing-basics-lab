6369944cb260ad047cf9ad8e34e187f067a26f1c
Derek Araujo <derekaraujo19@gmail.com>
2022-10-31 23:10:06 -0400

    commit tree 867ee7b99a1fc438f38cfbd0168559a2ca7dc60b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,18 @@
 class StudentsController < ApplicationController
+
   def index
     students = Student.all
     render json: students
   end
+
   def grades
     descGrades = Student.order(grade: :desc)
     render json: descGrades
   end
 
   def highest_grade
-    highest = Student.order(grade: :desc).first
-    render json: highest
+    student = Student.order(grade: :desc).first
+    render json: student
   end
 
 end
